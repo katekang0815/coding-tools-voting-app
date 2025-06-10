3e8ece113ea0e7499c1644fc42e12a4f52c082c0
YehsunKang <10900702-YehsunKang@users.noreply.replit.com>
2025-06-10 19:13:44 +0000

    Restored to '1f7c40fdb8ca316ffd4a751707f42efc56add39c'
    Replit-Restored-To: 1f7c40fdb8ca316ffd4a751707f42efc56add39c

--- client/src/components/ui/toast.tsx
+++ client/src/components/ui/toast.tsx
@@ -31,7 +31,7 @@ const toastVariants = cva(
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
         success:
-          "border-transparent text-white shadow-lg bg-[#66f04aa8]",
+          "border-transparent bg-[#16a397] text-white shadow-lg",
       },
     },
     defaultVariants: {
